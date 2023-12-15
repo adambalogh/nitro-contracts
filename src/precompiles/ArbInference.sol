ecc9232ede014b13f0c99ef3c145c6d2c3c81567
Matthew Wang <matthewwang2020@u.northwestern.edu>
2023-12-15 18:34:20 -0500

    ArbInferCall -> ArbInference

--- src/precompiles/ArbInference.sol
+++ src/precompiles/ArbInference.sol
@@ -1,8 +1,12 @@
+// Copyright 2021-2022, Offchain Labs, Inc.
+// For license information, see https://github.com/OffchainLabs/nitro-contracts/blob/main/LICENSE
+// SPDX-License-Identifier: BUSL-1.1
+
 pragma solidity >=0.4.21 <0.9.0;
 
 /// @title Infer Call
 /// @notice This contract is used to test the infer call functionality.
 /// This custom contract will set on 0x000000000000000000000000000000000000011a since we set it in precompile.go.
-interface ArbInferCall {
+interface ArbInference {
     function inferCall(bytes memory input) external returns (bytes memory);
 }
