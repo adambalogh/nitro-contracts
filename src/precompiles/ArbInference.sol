a96f757c58d8fa861c59665cdfdbde3731b3e232
Matthew Wang <matthewwang2020@u.northwestern.edu>
2023-12-18 18:11:27 -0500

    Inference ZK Solidity Interface

--- src/precompiles/ArbInference.sol
+++ src/precompiles/ArbInference.sol
@@ -5,8 +5,9 @@
 pragma solidity >=0.4.21 <0.9.0;
 
 /// @title Infer Call
-/// @notice This contract is used to test the infer call functionality.
+/// @notice This contract is used to run on-chain inference.
 /// This custom contract will set on 0x000000000000000000000000000000000000011a since we set it in precompile.go.
 interface ArbInference {
     function inferCall(bytes memory input) external returns (bytes memory);
+    function inferCallZK(bytes memory input) external returns (bytes memory);
 }
